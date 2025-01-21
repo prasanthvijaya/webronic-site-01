b967968d1fcf46c7ccf6f01fadd25d82b094abdf
prasanth <153979431+prasanthvijaya@users.noreply.github.com>
2025-01-21 19:12:12 +0530

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -13,19 +13,10 @@ import BlogsandCasestudies from '../components/HomeComponents/BlogsandCasestudie
 
 const Home=()=>{
     return(
-        <div className='flex flex-col '>
-            {/* <Navbar/> */}
-            <Header/>
-            <First_section/>
-            <Services/>
-            <BlogsandCasestudies/>
-            <Technologies/>
-            {/* <Ourservices/> */}
-            <Customers/>
-            <FaqSection />
-            <ContactForm/>
-            <Footer/>
-        </div>
+          <div>
+      <h1>Welcome to the Home Page!</h1>
+      <p>This is the home page content.</p>
+    </div>
     )
 }
 
